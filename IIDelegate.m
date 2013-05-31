dfcefd10bdb92686f44a2f87aa242d0445e7eab9
Shawn M Moore <code@sartak.org>
2013-05-31 13:17:27 -0400

    Throw error if required methods are missing

--- IIDelegate.m
+++ IIDelegate.m
@@ -14,7 +14,7 @@ static int instanceCount = 0;
     }
 }
 
-+(NSDictionary *)typeDictionaryFor:(Class) class {
++(NSDictionary *)typeDictionaryFor:(Class)class requiredMethods:(BOOL)requiredMethods {
     NSMutableDictionary *typeDictionary = [NSMutableDictionary dictionary];
 
     unsigned int protocolCount;
@@ -23,18 +23,7 @@ static int instanceCount = 0;
     for (int p = 0; p < protocolCount; ++p) {
         Protocol *protocol = protocolList[p];
         unsigned int methodCount;
-
-        // required methods
-        struct objc_method_description *descriptions = protocol_copyMethodDescriptionList(protocol, YES, YES, &methodCount);
-        for (int m = 0; m < methodCount; ++m) {
-            NSString *types = [NSString stringWithUTF8String:descriptions[m].types];
-            NSString *sel = NSStringFromSelector(descriptions[m].name);
-            [typeDictionary setObject:types forKey:sel];
-        }
-        free(descriptions);
-
-        // optional methods
-        descriptions = protocol_copyMethodDescriptionList(protocol, NO, YES, &methodCount);
+        struct objc_method_description *descriptions = protocol_copyMethodDescriptionList(protocol, requiredMethods, YES, &methodCount);
         for (int m = 0; m < methodCount; ++m) {
             NSString *types = [NSString stringWithUTF8String:descriptions[m].types];
             NSString *sel = NSStringFromSelector(descriptions[m].name);
@@ -47,14 +36,22 @@ static int instanceCount = 0;
 }
 
 +(void) addMethods:(NSDictionary *)methods to:(Class)class {
-    NSDictionary *typeDictionary = [self typeDictionaryFor:class];
+    NSDictionary *requiredTypes = [self typeDictionaryFor:class requiredMethods:YES];
+    NSDictionary *optionalTypes = [self typeDictionaryFor:class requiredMethods:NO];
+    NSMutableDictionary *requiredMethods = [requiredTypes mutableCopy];
 
     [methods enumerateKeysAndObjectsUsingBlock:^(NSString *methodName, id block, BOOL *stop) {
         SEL selector = NSSelectorFromString(methodName);
         IMP implementation = imp_implementationWithBlock(block);
-        NSString *types = [typeDictionary objectForKey:methodName];
+        NSString *types;
 
-        if (!types) {
+        if ((types = [requiredTypes objectForKey:methodName])) {
+            [requiredMethods removeObjectForKey:methodName];
+        }
+        else if ((types = [optionalTypes objectForKey:methodName])) {
+            // no action needed
+        }
+        else {
             [NSException raise:NSInternalInconsistencyException format:@"You may only add methods from the protocols you've declared; method %@ is not included in protocols <%@>", methodName, [[IIDelegate protocolsForClass:class] componentsJoinedByString:@", "]];
         }
 
@@ -63,6 +60,12 @@ static int instanceCount = 0;
             [NSException raise:NSInternalInconsistencyException format:@"Error adding method %@", methodName];
         }
     }];
+
+    // did they forget some required methods?
+    if ([requiredMethods count]) {
+        NSArray *methods = [requiredMethods keysSortedByValueUsingSelector:@selector(compare:)];
+        [NSException raise:NSInternalInconsistencyException format:@"Required protocol methods (%@) are missing", [methods componentsJoinedByString:@", "]];
+    }
 }
 
 +(id) delegateForProtocols:(NSArray *)protocols withMethods:(NSDictionary *)methods {
